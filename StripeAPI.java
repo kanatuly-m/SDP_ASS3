f4be126871acefb688ff0f39e8dd45ee664249a7
kanatuly-m <240941@astanait.edu.kz>
2025-09-30 15:29:48 +0500

    Delete StripeAPI.java

--- StripeAPI.java
+++
@@ -1,9 +0,0 @@
-public final class StripeAPI {
-    public String getName() { 
-        return "Stripe"; 
-    }
-
-    public void makeCharge(double cashUsd) {
-        System.out.println("[Stripe] Charged: $" + cashUsd);
-    }
-}
